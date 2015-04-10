79aa046b6a67b18a8a16bf1070e765af5f66fa9a
Ebanks <kbyyd24@outlook.com>
2015-04-10 22:36:51 +0800

    修复checker错误

--- tools/DBconnecter.php
+++ tools/DBconnecter.php
@@ -1,8 +1,10 @@
 <?php
 class DBconnecter {
 	const HOST = "mysql:host=121.199.57.37;dbname=Study_Man";
-	// 	const HOST_DB = "Study_Man";
-	// 	const HOST = "127.0.0.1";
+// 	const HOST = "mysql:host=127.0.0.1;dbname=study_man";
+// 	const HOST_DB = "Study_Man";
+// 	const HOST = "127.0.0.1";
 	const USER = "root";
 	const PASSWORD = "Cinsswpu123";
+// 	const PASSWORD = "";
 }
--- userInfo/checker.php
+++ userInfo/checker.php
@@ -5,16 +5,19 @@ header("Content-Type:text/xml;charset:UTF-8");
 $xmldata = file_get_contents('php://input');
 $data = (array)simplexml_load_string($xmldata);
 
+
 class ckecker{
 	private $userId;
 	private $time;
-	private $continouns;
-	private $result;
+	private $sub;
+	private $DBtime;
+	private $continouns = NULL;
+	private $result = NULL;
 	public $xml;
 	
 	function __construct($data) {
 		$this->userId = $data['userId'];
-		$this->time = strtotime(gmdate("D, d M Y", strtotime("+8 hours")));
+		$this->time = gmdate("D, d M Y", strtotime("+128 hours"));
 	}
 	
 	private function check() {
@@ -22,35 +25,50 @@ class ckecker{
 		$conn = new PDO(DBconnecter::HOST, DBconnecter::USER, DBconnecter::PASSWORD);
 		$DBdata = $conn->query($sql);
 		while ($row = $DBdata->fetch()) {
-			$time = strtotime($row['time']);
+			$this->DBtime = $row['time'];
 			$continouns = $row['continouns'];
-			$subtract = floor((strtotime($this->time)-$time)/3600/24);
-			if ($subtract == 0) {
+			$this->sub = floor((strtotime($this->time)-$this->DBtime)/3600/24);
+			if ($this->sub == 0) {
 				$this->result = "checked today";
 				$this->continouns = $continouns;
-			}elseif ($subtract == 1) {	
+			}elseif ($this->sub == 1) {	
 				$this->result = "check success";
 				if (empty($continouns) || $continouns == 0) {
 					$this->continouns = 1;
 				} else {
 					$this->continouns = $continouns+1;
 				}
-			}elseif ($subtract > 1) {
+				$this->save();
+			}elseif ($this->sub > 1) {
 				$this->result = "check success";
 				$this->continouns = 1;
+				$this->save();
 			}else {
 				$this->result = "unknow error";
-				$this->continouns = null;
+				$this->continouns = -1;
 			}
 		}
-		$sql = "INSERT INTO `check`(`userId`,`time`,`continouns`) VALUES ('$this->userId', '$this->time', '$this->continouns')";
-		$conn->exec($sql);
+		if (empty($this->result)||empty($this->continouns)) {
+			$this->result = "first check";
+			$this->continouns = 1;
+			$this->save();
+		}
+	}
+	
+	private function save() {
+		$conn = new PDO(DBconnecter::HOST, DBconnecter::USER, DBconnecter::PASSWORD);
+		$sql = "INSERT INTO `check`(`userId`,`time`,`continouns`) VALUES ('$this->userId', '".strtotime($this->time)."', '$this->continouns')";
+		$conn->exec($sql);	
 	}
 	
 	private function create_xml() {
 		$this->xml = "<?xml version='1.0' encoding='UTF-8'?><root>";
 		$this->xml .= "<result>$this->result</result>";
 		$this->xml .= "<continouns>$this->continouns</continouns>";
+		$this->xml .= "<time>$this->time</time>";
+		$this->xml .= "<sub>$this->sub</sub>";
+		$this->xml .= "<dbt>$this->DBtime</dbt>";
+		$this->xml .= "<strt>".strtotime($this->time)."</strt>";
 		$this->xml .= "</root>";
 	}
 	
