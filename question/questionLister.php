9a6572f4cf387bf35754e5c564e9e1c3481c2e8c
Ebanks <kbyyd24@outlook.com>
2015-04-15 15:22:13 +0800

    简化问题列表查询方式，并修改questionLister的result为int

--- question/questionLister.php
+++ question/questionLister.php
@@ -5,17 +5,17 @@ header("Content-Type:text/xml;charset:UTF-8");
 $xmldata = file_get_contents('php://input');
 $data = (array)simplexml_load_string($xmldata);
 class questionLister{
-	private $friendId;
+	private $userId;
+	private $searcherId;
 	private $page;
 	private $number;
-	private $name;
 	private $DBdata;
-	private $nameResult;
-	private $questionResult;
+	private $result = 0;
 	public $xml;
 	
 	function __construct($data) {
-		$this->friendId = $data['friendId'];
+		$this->userId = $data['userId'];
+		$this->searcherId = $data['searcherId'];
 		$this->page = $data['page'];
 		if (array_key_exists('number', $data)) {
 			$this->number = $data['number'];;
@@ -31,38 +31,34 @@ class questionLister{
 	}
 	
 	private function search_name() {
-		$sql = "SELECT `name` FROM `users_infomation` WHERE `id`='$this->friendId'";
+		$sql = "SELECT `name` FROM `users_infomation` WHERE `id`='$this->searcherId'";
 		$conn = new PDO(DBconnecter::HOST, DBconnecter::USER, DBconnecter::PASSWORD);
 		$DBdata = $conn->query($sql);
 		while ($row = $DBdata->fetch()) {
 			$this->name = $row['name'];
-			$this->nameResult = "user is exist";
 		}
 		if (empty($this->name)) {
-			$this->nameResult = "user is not exist";
+			$this->result = 1;
 		}
 	}
 	
 	private function search_question() {
 		$lowest = ($this->page-1)*$this->number;
 		$highest = $this->page*$this->number;
-		$sql = "SELECT `questionId`,`title`,`time`,`answerNumber`,`adoptId` FROM `questions` WHERE `userId`='$this->friendId' LIMIT $lowest,$highest";
+		$sql = "SELECT `questionId`,`title`,`time`,`answerNumber`,`adoptId` FROM `questions` WHERE `userId`='$this->searcherId' LIMIT $lowest,$highest";
 		$conn = new PDO(DBconnecter::HOST, DBconnecter::USER, DBconnecter::PASSWORD);
 		$this->DBdata = $conn->query($sql);
 		$row = $this->DBdata->fetch();
 		if (empty($row)) {
-			$this->questionResult = "no question exist";
+			$this->result += 2;
 		} else {
 			$this->DBdata = $conn->query($sql);
-			$this->questionResult = "success";
 		}
 	}
 	
 	private function create_xml() {
 		$this->xml = "<?xml version='1.0' encoding='UTF-8'?><root>";
-		$this->xml .= "<nameResult>$this->nameResult</nameResult>";
-		$this->xml .= "<questionResult>$this->questionResult</questionResult>";
-		$this->xml .= "<name>$this->name</name>";
+		$this->xml .= "<result>$this->result</result>";
 		while($row = $this->DBdata->fetch()){
 			$this->xml .= "<question>";
 			$this->xml .= "<questionId>".$row['questionId']."</questionId>";
@@ -78,9 +74,6 @@ class questionLister{
 	public function work() {
 		$this->search_name();
 		$this->search_question();
-		if ($this->nameResult == "user is not exist") {
-			$this->questionResult = $this->nameResult;
-		}
 		$this->create_xml();
 	}
 }
